f81c2454e1cdc1d387c4d8a060e5abfd3835ce64
Developer <dev@example.com>
2026-01-07 14:08:10 -0500

    render ready yaml

--- disc_entr_project/disc_entr_project/settings.py
+++ disc_entr_project/disc_entr_project/settings.py
@@ -9,7 +9,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key-change-this-in-production')
+SECRET_KEY = os.environ.get('SECRET_KEY', '366536653665')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
